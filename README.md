77a240d3a6d1bb22cd8b989d190df056e31e4e86
ThibaudMallet <thibaud.mallet@hotmail.fr>
2021-12-29 17:47:48 +0100

    réalisation d'un compte à rebours en suivant un tuto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CompteARebours
+
+Le but ici est de créer un compte à rebours et de commencer à manipuler les fonctions liées au temps !
---
+++ app.js
@@ -0,0 +1,17 @@
+let temps = 100;
+
+const timerElement = document.getElementById("timer")
+
+function diminuerTemps() {
+    let minutes = parseInt(temps / 60, 10);
+    let secondes = parseInt(temps % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    secondes = secondes < 10 ? "0" + secondes : secondes;
+
+    timerElement.innerText = minutes + ":" + secondes;
+    temps = temps <=0 ? 0 : temps - 1;
+}
+setInterval(diminuerTemps, 1000);
+
+
